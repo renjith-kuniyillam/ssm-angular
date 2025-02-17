5d1394ba694590348671a11c54950ae70338c0a7
Z004RY7V <renjith.k@siemens.com>
2025-02-17 21:40:24 +0530

    commiting the latest changes

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EChartsCoreOption } from 'echarts';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  chartOption: EChartsCoreOption  = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow' // Use 'line' or 'shadow' based on your preference
+      }
+    },
+    xAxis: {
+      type: 'value'
+    },
+    yAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+        itemStyle: {
+          color: '#05c091' // Change this to your desired color
+        }
+      }
+    ],
+   
+  };
 
   constructor() { 
     console.log('Home constructor');
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { NgxEchartsModule } from 'ngx-echarts';
+import * as echarts from 'echarts';
 
 const homeRoutes: Routes = [
   { path: '', component: HomeComponent }
@@ -12,7 +14,8 @@ const homeRoutes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(homeRoutes),
-    MatCardModule
+    MatCardModule,
+    NgxEchartsModule.forRoot({ echarts }),
   ],
   declarations: [HomeComponent]
 })
